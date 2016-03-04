8d0d4e5ed607d2bbf3d0dd345b1409266d798f2e
IMSSAM <codingssam@gmail.com>
2016-03-04 16:21:55 +0900

    job_name 컬럼 추가

--- sql/t_schedules.sql
+++ sql/t_schedules.sql
@@ -1,5 +1,6 @@
 CREATE TABLE t_schedules (
   id int(11) NOT NULL AUTO_INCREMENT,
+  job_name varchar(100) DEFAULT NULL,
   exec_time_utc datetime DEFAULT NULL,
   exec_status varchar(10) DEFAULT NULL,
   exec_result_log varchar(255) DEFAULT NULL,
