b2787c422bc99250a2e647551c82f6a7f6ba3cb5
IMSSAM <codingssam@gmail.com>
2016-03-04 13:29:03 +0900

    exec_status, exec_result_log 컬럼 추가

--- sql/t_schedules.sql
+++ sql/t_schedules.sql
@@ -1,5 +1,7 @@
 CREATE TABLE t_schedules (
   id int(11) NOT NULL AUTO_INCREMENT,
   exec_time_utc datetime DEFAULT NULL,
+  exec_status varchar(10) DEFAULT NULL,
+  exec_result_log varchar(255) DEFAULT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
