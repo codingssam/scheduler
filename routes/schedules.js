1dae6a7e9fff91f241f33ccca8641376d70fe483
IMSSAM <codingssam@gmail.com>
2016-03-04 13:32:23 +0900

    스케쥴링 작업을 위한 URL schedules/test, schedules/testrecur의 POST 요청 처리 코드 추가

--- routes/schedules.js
+++ routes/schedules.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../config/dbpool');
 var moment = require('moment-timezone');
+var nodeschedule = require('node-schedule');
 
 router.post('/', function(req, res, next) {
 	var year = parseInt(req.body.year);
@@ -15,8 +16,8 @@ router.post('/', function(req, res, next) {
 	var m = moment({"year": year, "month": month, "day": day,
 								  "hour": hour, "minute": minute, "second": second}).tz(timezone);
 
-	var tzTime = m.format("YYYY-MM-DD HH-mm-ss");
-	var utcTime = m.utc().format("YYYY-MM-DD HH-mm-ss");
+	var tzTime = m.format("YYYY-MM-DD HH:mm:ss");
+	var utcTime = m.utc().format("YYYY-MM-DD HH:mm:ss");
 	pool.getConnection(function(err, connection) {
 		if (err) {
 			next(err);
@@ -59,6 +60,43 @@ router.get('/now', function(req, res, next) {
       });
     }
   });
+});
+
+router.post('/test', function(req, res, next) {
+	var year = parseInt(req.body.year);
+	var month = parseInt(req.body.month) - 1;
+	var day = parseInt(req.body.day);
+	var hour = parseInt(req.body.hour);
+	var minute = parseInt(req.body.minute);
+	var second = parseInt(req.body.second);
+
+	var m = moment({"year": year, "month": month, "day": day,
+								  "hour": hour, "minute": minute, "second": second}).tz('Asia/Seoul');
+
+	var date = m.toDate();
+	var job = nodeschedule.scheduleJob(date, function(mm) {
+		console.log(mm.format("YYYY-MM-DD HH:mm:ss") + "에 실행됨...");
+		console.log(mm.utc().format("YYYY-MM-DD HH:mm:ss") + "에 실행됨...");
+	}.bind(null, m));
+
+	res.json({
+		"message": m.format("YYYY-MM-DD HH:mm:ss") + "에 실행될 작업을 요청하였음..."
+	});
+
+});
+
+router.post('/testrecur', function(req, res, next) {
+	var cronstyle = req.body.cronstyle;
+
+	var job = nodeschedule.scheduleJob(cronstyle, function() {
+		console.log(moment().format("YYYY-MM-DD HH:mm:ss") + "에 주기작업이 실행됨...");
+		console.log(moment().utc().format("YYYY-MM-DD HH:mm:ss") + "에 주기작업이 실행됨...");
+	});
+
+	res.json({
+		"message": "주기적으로 실행될 작업을 요청하였음..."
+	});
+
 });
 
 module.exports = router;
